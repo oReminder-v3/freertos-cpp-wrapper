169e2c91f40ec0e9d9fbe634bef70690a16a72c9
robcholz <84130577+robcholz@users.noreply.github.com>
2024-11-09 18:16:32 -0500

    [bugfix] fixed message channel

--- include/freertos-cpp-wrapper/MessageChannel.hpp
+++ include/freertos-cpp-wrapper/MessageChannel.hpp
@@ -16,55 +16,86 @@
 
 namespace freertos {
 namespace internal {
-template <typename T>
+template<typename T>
 class MessageChannelBase {
- public:
+public:
   explicit MessageChannelBase(MessageBufferHandle_t messageBuffer)
-      : messageBuffer(messageBuffer) {}
-  virtual ~MessageChannelBase() { vMessageBufferDelete(messageBuffer); }
+	  : messageBuffer(messageBuffer) {}
+
+  // Destructor: Cleanup the message buffer resource
+  virtual ~MessageChannelBase() {
+	if (messageBuffer != nullptr) {
+	  vMessageBufferDelete(messageBuffer);
+	}
+  }
+
+  // Disable copy constructor and copy assignment operator
+  MessageChannelBase(const MessageChannelBase &) = delete;
+  MessageChannelBase &operator=(const MessageChannelBase &) = delete;
+
+  // Enable move constructor and move assignment operator
+  MessageChannelBase(MessageChannelBase &&other) noexcept
+	  : messageBuffer(other.messageBuffer) {
+	other.messageBuffer = nullptr;
+  }
+
+  MessageChannelBase &operator=(MessageChannelBase &&other) noexcept {
+	if (this != &other) {
+	  if (messageBuffer != nullptr) {
+		vMessageBufferDelete(messageBuffer);
+	  }
+	  messageBuffer = other.messageBuffer;
+	  other.messageBuffer = nullptr;
+	}
+	return *this;
+  }
 
   [[nodiscard]] FREERTOS_ALWAYS_INLINE bool hasElement() const {
-    return xMessageBufferIsEmpty(messageBuffer) == false;
+	assert(messageBuffer != nullptr);
+	return xMessageBufferIsEmpty(messageBuffer) != pdTRUE;
   }
 
   void sendMessage(T message) {
-    T t = std::move(message);
-    xMessageBufferSend(messageBuffer, &t, sizeof(T), portMAX_DELAY);
+	assert(messageBuffer != nullptr);
+	static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
+	T t = std::move(message);
+	xMessageBufferSend(messageBuffer, &t, sizeof(T), portMAX_DELAY);
   }
 
   std::optional<T> getMessage() {
-    if (!hasElement())
-      return std::nullopt;
-    T t{};
-    xMessageBufferReceive(messageBuffer, &t, sizeof(T), portMAX_DELAY);
-    return std::move(t);
+	if (!hasElement()) {
+	  return std::nullopt;
+	}
+	T t{};
+	xMessageBufferReceive(messageBuffer, &t, sizeof(T), portMAX_DELAY);
+	return t;
   }
 
- private:
+private:
   MessageBufferHandle_t messageBuffer{};
 };
-}  // namespace internal
+}// namespace internal
 
-template <typename T, size_t SIZE>
+template<typename T, size_t SIZE>
 class StaticMessageChannel : public internal::MessageChannelBase<T> {
- public:
+public:
   explicit StaticMessageChannel()
-      : internal::MessageChannelBase<T>(
-            xMessageBufferCreateStatic(sizeof(messageBufferStorage),
-                                       messageBufferStorage,
-                                       &messageBufferStruct)) {}
+	  : internal::MessageChannelBase<T>(
+			xMessageBufferCreateStatic(sizeof(messageBufferStorage),
+									   messageBufferStorage,
+									   &messageBufferStruct)) {}
 
- private:
+private:
   uint8_t messageBufferStorage[SIZE * sizeof(T)]{};
   StaticMessageBuffer_t messageBufferStruct{};
 };
 
-template <typename T, size_t SIZE>
+template<typename T, size_t SIZE>
 class DynamicMessageChannel : public internal::MessageChannelBase<T> {
- public:
+public:
   explicit DynamicMessageChannel()
-      : internal::MessageChannelBase<T>(xMessageBufferCreate(sizeof(T) * SIZE)) {}
+	  : internal::MessageChannelBase<T>(xMessageBufferCreate(sizeof(T) * SIZE)) {}
 };
-}  // namespace utils
+}// namespace freertos
 
-#endif  // FREERTOS_CPP_WRAPPER_INCLUDE_MESSAGECHANNEL_HPP
+#endif// FREERTOS_CPP_WRAPPER_INCLUDE_MESSAGECHANNEL_HPP
